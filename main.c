61a5a6d3099db6694e2e588d360dda66683f4869
coder-jkb <jkb17codes@gmail.com>
2021-08-17 14:24:48 +0530

    Menu created and resources declared

---
+++ main.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(){
+    char choice;
+    printf("\t     ,-'-.                                           \n");
+    printf("\t   _r-----i          _                                   \n");
+    printf("\t  \\      |-.       ,###.                                 \n");
+    printf("\t    |     | |    ,-------.                               \n");
+    printf("\t    |     | |   c|       |                       ,--.    \n");
+    printf("\t    |     |'     |       |      _______________ C|  |    \n");
+    printf("\t    (=====)      =========      \\_____________/  `=='    \n");
+    printf("\n\n\t\t\t!!! Welcome !!!\n");
+    printf("\t\tWhat would you like to have ?\n");
+    printf("\t\t\t MENU :\n");
+    printf("\t\t\t 1. Regular Tea\n");
+    printf("\t\t\t 2. Special Tea\n");
+    printf("\t\t\t 3. Ice Tea Tea\n");
+    printf("\t\t\t 4. Cappuccino\n");
+    printf("\t\t\t 5. Latte\n");
+    printf("\t\t\t 6. Espresso\n");
+    printf("\t\t\t 7. Cold Milk\n");
+    printf("\t\t\t 8. Hot Milk\n");
+    printf("\t\t\t 9. Hot Chocolate\n");
+
+    // Resources availabe:
+    int totalMilk = 20000; // 20000 ml 
+    int totalSugar = 5000; // 5000g 
+    int coffeePowder = 5000; // 5000 g
+    int teaPowder = 5000; // 5000 g
+    int chocolate = 2000; // 2000 g
+
+}
+
