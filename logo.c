052b3229b3465b6ae904bb464cebeeb843185af1
PrachetShah <prachetshah25@gmail.com>
2021-08-17 14:11:39 +0530

    logo added

---
+++ logo.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    printf("         ,-'-.                                           \n");
+    printf("   _r-----i          _                                   \n");
+    printf("   \      |-.      ,###.                                 \n");
+    printf("    |     | |    ,-------.                               \n");
+    printf("    |     | |   c|       |                       ,--.    \n");
+    printf("    |     |'     |       |      _______________ C|  |    \n");
+    printf("    (=====)      =========      \_____________/  `=='    \n");
+}
